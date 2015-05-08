c55881b0ec47c0fede65912e9d7d830415939d15
SignalSin <michaelhenderson.it@gmail.com>
2015-05-08 18:52:07 +0800

    Added Last Refresh timestamp

--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -4,6 +4,8 @@ app.controller('MainController', ['$scope', 'forecast', function($scope, forecas
     $scope.loadData= function() {
           forecast.success(function(data) {
   	$scope.forecast = data;
+    var date = new Date;
+    $scope.timestamp = date.toLocaleString();
   });
     }
     
